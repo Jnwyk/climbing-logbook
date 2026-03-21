e87611443e4db736bb72ae1073d0abb5e4f4d8c1
Jnwyk <jan.nowak.personal@gmail.com>
2026-03-21 10:15:16 +0100

    add route cards

---
+++ client/src/interfaces/RouteCardInterface.ts
@@ -0,0 +1,7 @@
+export interface RouteCardInterface {
+  name: string;
+  crag: string;
+  area: string;
+  country: string;
+  grade: string;
+}
--- client/src/pages/LogbookPage.tsx
+++ client/src/pages/LogbookPage.tsx
@@ -1,5 +1,15 @@
+import RouteCard from '../components/RouteCard';
+
 function LogbookPage() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col gap-4 pt-16 px-8 mx-auto max-w-7xl w-full">
+      <RouteCard />
+      <RouteCard />
+      <RouteCard />
+      <RouteCard />
+      <RouteCard />
+    </main>
+  );
 }
 
 export default LogbookPage;
